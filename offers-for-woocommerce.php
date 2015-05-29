fc0fdff963c0e3935f97b9cca1f39b3b7f9660ed
kcwebmedia <cole@kcwebmedia.com>
2015-05-29 02:42:38 -0500

    Moves load_plugin_textdomain to plugin base. Adds error notice if error loading i18n file. Ref#159.

--- offers-for-woocommerce.php
+++ offers-for-woocommerce.php
@@ -36,6 +36,40 @@ if(!defined('ABSPATH'))
  */
 require_once( plugin_dir_path( __FILE__ ) . 'public/class-offers-for-woocommerce.php' );
 
+
+/**
+ * Load plugin text domain
+ */
+add_action('plugins_loaded', 'angelleye_ofwc_load_plugin_textdomain');
+
+/**
+ * Load the plugin text domain for translation
+ *
+ * @since    1.1.3
+ */
+function angelleye_ofwc_load_plugin_textdomain()
+{
+    $plugin = Angelleye_Offers_For_Woocommerce::get_instance();
+    $plugin_slug = $plugin->get_plugin_slug();
+    $locale = apply_filters( 'plugin_locale', get_locale(), $plugin_slug );
+    $plugin_rel_path = plugin_dir_path( __FILE__ );
+    $mo_file_path = $plugin_rel_path . 'languages/'.$plugin_slug.'-'. $locale . '.mo';
+
+    if($locale != 'en_US')
+    {
+        $loadplugintextdomain = load_plugin_textdomain( $plugin_slug, FALSE, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+        if(!$loadplugintextdomain)
+        {
+            if( file_exists($mo_file_path) )
+            {
+                echo '<div class="notice error">';
+                echo '<p>'. __('<strong>Error: </strong>Language translation file could not be loaded', $plugin_slug). '</p>';
+                echo '</div>';
+            }
+        }
+    }
+}
+
 /**
  * Register hooks that are fired when the plugin is activated or deactivated
  * When the plugin is deleted, the uninstall.php file is loaded
