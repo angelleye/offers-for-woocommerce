b132b0943f790ac05e33161beb42d660addc0bd8
kcwebmedia <cole@kcwebmedia.com>
2015-06-27 06:48:32 -0500

    Adds billing name, company name, phone user data. Helps to resolve #160.

--- public/class-offers-for-woocommerce.php
+++ public/class-offers-for-woocommerce.php
@@ -469,14 +469,30 @@ class Angelleye_Offers_For_Woocommerce {
         }
 
         // If name,phone,email not already specified, then try to pull logged in user data if user is logged in
-        if( is_user_logged_in() )
+        if(empty($offer_name))
         {
-            $current_user = wp_get_current_user();
-
-            if(empty($offer_name))
+            if( is_user_logged_in() )
             {
-                $offer_name = $current_user->user_firstname . ' ' . $current_user->user_lastname;
+                $current_user = wp_get_current_user();
+
                 $offer_email = $current_user->user_email;
+                $offer_company_name = (!empty($current_user->billing_company)) ? $current_user->billing_company : '';
+                $offer_phone = (!empty($current_user->billing_phone)) ? $current_user->billing_phone : '';
+
+                // use WP user first/last name if available
+                if(!empty($current_user->user_firstname))
+                {
+                    $offer_name = $current_user->user_firstname;
+                    $offer_name.= (!empty($current_user->user_lastname)) ? ' ' . $current_user->user_lastname : '';
+                }
+                else // use WP/WC billing first/last name
+                {
+                    if(!empty($current_user->billing_first_name))
+                    {
+                        $offer_name = $current_user->billing_first_name;
+                        $offer_name.= (!empty($current_user->billing_last_name)) ? ' ' . $current_user->billing_last_name : '';
+                    }
+                }
             }
         }
 
