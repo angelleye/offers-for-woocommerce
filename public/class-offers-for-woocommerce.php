c612eded33864c9b8152c7c32f4896455dd63817
kcppdevelopers <kcppdevelopers@gmail.com>
2015-10-15 18:56:03 +0530

    add grey background, ref #7

--- public/class-offers-for-woocommerce.php
+++ public/class-offers-for-woocommerce.php
@@ -1959,8 +1959,9 @@ class Angelleye_Offers_For_Woocommerce {
         if ($this->ofw_is_mailling_list_enable() && !$is_counter_offer) {
             ?>
             <div class="woocommerce-make-offer-form-section wideBox25px">
-                <label for="join_our_mailing_list"><input name="join_our_mailing_list" id="join_our_mailing_list" type="checkbox" value="yes" checked="checked"><?php echo apply_filters('aeofwc-offer-form-label-join-our-mailing-list', __('Join our Mailing List', $this->plugin_slug)); ?></label>
+                <input name="join_our_mailing_list" id="join_our_mailing_list" type="checkbox" value="yes" checked="checked"><label for="join_our_mailing_list" class="checkbox"><?php echo str_repeat('&nbsp;', 1); ?> <?php echo apply_filters('aeofwc-offer-form-label-join-our-mailing-list', __('Join Our Mailing List', $this->plugin_slug)); ?></label>
             </div>
+            
             <?php
         }
     }
