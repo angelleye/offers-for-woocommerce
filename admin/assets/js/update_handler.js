4a123a1d94543328169d2bfabe2fa18e28d28bf8
Deepak Maurya <deepakmaurya@hotmail.com>
2020-02-28 14:54:20 +0530

    Updater message dismiss issue resolved, OFW-139

---
+++ admin/assets/js/update_handler.js
@@ -0,0 +1,25 @@
+(function ( $ ) {
+    "use strict";
+
+    $(function () {
+        $(document).off('click', '#angelleye-updater-notice .notice-dismiss').on('click', '#angelleye-updater-notice .notice-dismiss',function(event) {
+            var r = confirm("If you do not install the Updater plugin you will not receive automated updates for Angell EYE products going forward!");
+            if (r == true) {
+                var data = {
+                    action : 'angelleye_updater_dismissible_admin_notice'
+                };
+                $.post(ajaxurl, data, function (response) {
+                    var $el = $( '#angelleye-updater-notice' );
+                    event.preventDefault();
+                    $el.fadeTo( 100, 0, function() {
+                        $el.slideUp( 100, function() {
+                            $el.remove();
+                        });
+                    });
+                });
+            }
+        });
+
+    });
+}(jQuery));
+
